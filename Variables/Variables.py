35d0ffe1e33bd1b5466df459f3e4d61a60c1061e
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-09-08 00:30:58 +0530

    Create new files

---
+++ Variables/Variables.py
@@ -0,0 +1,8 @@
+urlOfIntranet = "https://intranet.einfochips.com/intranets/auth/login"
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+ascendingLetters = letters.copy()
+letters.reverse()
+
+headerName = ['Name', 'Reporting', 'Designation', 'Experience', 'Phone Number', 'Ext. No.', 'Department', 'Email Id', 'Location', 'Skills']
