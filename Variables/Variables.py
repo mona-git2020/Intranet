1b95aa21904f7081b06d5e2ab30b5c2ffe6dc36c
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-10-05 19:12:28 +0530

    Completed TC10 & TC11

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,9 +1,10 @@
-urlOfIntranet = "https://intranet.einfochips.com/intranets/auth/login"
+urlOfIntranet = "https://intranet.einfochips.com/employee_intranet/portal/auth/login"
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 ascendingLetters = letters.copy()
 letters.reverse()
 
-headerName = ['Name', 'Reporting', 'Designation', 'Experience', 'Phone Number', 'Ext. No.', 'Department', 'Email Id', 'Location', 'Skills']
-
+headerName = ['Name', 'Reporting', 'Designation', 'Experience', 'Phone Number', 'Department', 'Email Id', 'Location']
+profileAddress = "C:/Users/AMAN/AppData/Roaming/Mozilla/Firefox/Profiles/90yqdr7b.default"
+downloadDir = "D:/MONU/Robotframework/Intranet/Downloads"
